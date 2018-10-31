6ec6407ea984ef07d46f56a422d01aca6325c17e
Sergii Bondarenko <sergibondarenko@gmail.com>
2018-10-31 15:33:10 +0100

    Bump version. Correctly require modules.

--- public/app.js
+++ public/app.js
@@ -1,16 +1,10 @@
-import uiModules from 'ui/modules';
-import uiRoutes from 'ui/routes';
-
-import rootTemplate from './templates/root_template.html';
-
-uiRoutes.enable();
-uiRoutes
-.when('/', {
-  template: rootTemplate,
+require('ui/routes').enable();
+require('ui/routes').when('/', {
+  template: require('plugins/alert/templates/root_template.html'),
   controller: 'RootController',
 });
 
-const app = uiModules.get('apps/alert', []);
+const app = require('ui/modules').get('apps/alert', []);
 app.controller('RootController', function ($scope) {
   $scope.description = 'alert';
 });
